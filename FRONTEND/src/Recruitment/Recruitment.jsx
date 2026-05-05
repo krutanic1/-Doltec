19275cc8ecf37bfe91bbbd1253c81d5008e0ccd3
Tarun Sai k <tarunsaikolaa@gmail.com>
2026-05-05 15:21:34 +0530

    fixes

--- FRONTEND/src/Recruitment/Recruitment.jsx
+++ FRONTEND/src/Recruitment/Recruitment.jsx
@@ -1,11 +1,8 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import ToggleComponent from "../Components/ToggleComponent";
 // import Landing from "./Landing";
 import Landingpage from "./Landingpage";
 const Recruitment = () => {
-  const [showContactPopup, setShowContactPopup] = useState(false);
-
   return (
     <div id="demo">
     <div className="demo__content">
@@ -73,40 +70,17 @@ const Recruitment = () => {
       </div>   
       <div className="recruitment-intro">              
         <div className="intro-actions">
-          <button type="button" className="intro-action-btn" onClick={() => setShowContactPopup(true)}>
+          <a
+            href="https://www.doltec.in/ContactUs"
+            className="intro-action-btn"
+          >
             Connect <i className="fa fa-chevron-circle-right"></i>
-          </button>
+          </a>
           <Link to="/real-estate" className="intro-action-btn">
             Real Estate <i className="fa fa-building"></i>
           </Link>
         </div>
       </div>
-
-      {showContactPopup && (
-        <div className="contact-popup-overlay" onClick={() => setShowContactPopup(false)}>
-          <div className="contact-popup" onClick={(event) => event.stopPropagation()}>
-            <button
-              type="button"
-              className="contact-popup-close"
-              onClick={() => setShowContactPopup(false)}
-            >
-              ×
-            </button>
-            <div className="contact-popup-title">Connect</div>
-            <div className="contact-popup-name">Suryansh</div>
-            <div className="contact-popup-number">+91 93245 04318</div>
-            <a
-              className="contact-popup-whatsapp"
-              href="https://wa.me/919324504318"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Open WhatsApp"
-            >
-              <i className="fa fa-whatsapp"></i>
-            </a>
-          </div>
-        </div>
-      )}
     </div>
     <Landingpage/>
     </div>
